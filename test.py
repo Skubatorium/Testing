ef5e1f539110e888baa6c7dfe0d31e61d9f31aa4
Skubatorium <Skubi80@googlemail.com>
2015-10-26 23:06:29 +0100

    Beautifying

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ import random
 
 
 def fakeLoader():
-    # Useless Loading "Animation"
+    # Useless Loading "Animation" :)
     countingNumber = 0
     linebreaks = "\n" * 100
     print(linebreaks)
     for i in range(1,100):
         if countingNumber < 100:
             print("TestMania 0.2a" + "\n")
-            countingNumber = countingNumber + random.randrange(30,random.randrange(60,90))
+            countingNumber = countingNumber + random.randrange(20,random.randrange(30,50))
             if countingNumber < 100:
                 print("Loading: " + str(countingNumber) + "%")
                 time.sleep(random.randrange(1,2))
@@ -24,39 +24,39 @@ def fakeLoader():
 
 
 def split_sentence(inputText):
-    # Split the Text into a String-List and return
+    # Split the inputText into a string-list and return it
     splitted_sentence = inputText.split()
     return splitted_sentence
 
 
 def scramble_word(splitted_sentence):
-    # Shuffle every single word, keep first and las char and return
+    # Shuffle every single word,
+    # keep first and last char and return the sentence
     final_sentence = []
     for i in splitted_sentence:
         middleLetterList = []
         buildWord = []
         finalWord = []
         for (j, letter) in enumerate(i):
-            wordMaxLengthID = len(i) - 1
+            wordLetterCount = len(i) - 1
             if j == 0:
                 firstChar = letter
                 buildWord.append(firstChar)
-            elif j > 0 and j < wordMaxLengthID:
+            elif j > 0 and j < wordLetterCount:
                 middleLetterList.append(letter)
-                if len(middleLetterList) == wordMaxLengthID - 1:
+                if len(middleLetterList) == wordLetterCount - 1:
                     random.shuffle(middleLetterList)
                     buildWord.append(''.join(middleLetterList))
-            elif j == wordMaxLengthID:
+            elif j == wordLetterCount:
                 lastChar = letter
                 buildWord.append(lastChar)
-                for flattenWord in buildWord:
-                    finalWord.append(''.join(flattenWord))
+                for k in buildWord:
+                    finalWord.append(''.join(k))
                 final_sentence.append(''.join(finalWord))
-        return final_sentence
+    return final_sentence
 
 
-
-if __name__ == "__main__":   
+if __name__ == "__main__":
     fakeLoader()
     inputText = input("Enter Text: ")
     splitted_sentence = split_sentence(inputText)
